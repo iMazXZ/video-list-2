2a329fd549d6043a3bb5f774739251470a2d192e
iMazXZ <akungamemaulana@gmail.com>
2025-07-09 21:05:45 +0700

    Histats

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer"; // 1. Impor komponen Footer
+import Footer from "@/components/Footer";
 import Providers from "@/app/providers";
+import Script from "next/script"; // 1. Impor komponen Script
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,8 +28,33 @@ export default function RootLayout({
             <Navbar />
             <main>{children}</main>
           </div>
-          <Footer /> {/* 2. Tambahkan komponen Footer di sini */}
+          <Footer />
         </Providers>
+
+        {/* 2. Tambahkan skrip Histats di sini */}
+        <Script id="histats-script" strategy="lazyOnload">
+          {`
+            var _Hasync= _Hasync|| [];
+            _Hasync.push(['Histats.start', '1,4962377,4,0,0,0,00010000']);
+            _Hasync.push(['Histats.fasi', '1']);
+            _Hasync.push(['Histats.track_hits', '']);
+            (function() {
+            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+            hs.src = ('//s10.histats.com/js15_as.js');
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+            })();
+          `}
+        </Script>
+        <noscript>
+          <a href="/" target="_blank">
+            <img
+              src="//sstatic1.histats.com/0.gif?4962377&101"
+              alt="Histats"
+              style={{ border: 0 }}
+            />
+          </a>
+        </noscript>
+        {/* Histats.com END */}
       </body>
     </html>
   );
